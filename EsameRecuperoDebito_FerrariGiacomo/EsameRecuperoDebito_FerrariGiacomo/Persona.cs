33822ecc48cb33f9312e3fa29dc8768aec042d16
Pascal Informatica <informatica.pascal@comandini.local>
2025-09-02 15:21:10 +0200

    Classe Persona 1.0

--- EsameRecuperoDebito_FerrariGiacomo/EsameRecuperoDebito_FerrariGiacomo/Persona.cs
+++ EsameRecuperoDebito_FerrariGiacomo/EsameRecuperoDebito_FerrariGiacomo/Persona.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,33 @@ namespace EsameRecuperoDebito_FerrariGiacomo
 {
     internal class Persona
     {
+        public string nome;
+        public string cognome;
+        int numeroCarta;
+        int passwordCarta;
+        string email;
+        string passwordEmail;
+
+
+        class Utente
+        {
+            int pinUtente;
+            // Relativi metodi della classe Utente
+        }
+        class Amministratore
+        {
+            string emailAmministratore; // Email aggiuntiva (oltre a quella personale) del sito di compravendita dei mezzi (con relativa password)
+            string paswordEmailAmministratore;
+            static bool cambiaEmailAmministratore(string emailAmministratore)
+            {
+                return true;
+            }
+            // Relativi altri metodi della classe Amministratore
+        }
+        static bool cambiaPassword(string password)
+        {
+            return true;
+        }
+        // Relativi alrti metodi della classe Persona
     }
 }
