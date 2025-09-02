cde99a81976793eb926c953a5b705ad01c312c7e
Pascal Informatica <informatica.pascal@comandini.local>
2025-09-02 15:27:48 +0200

    Creazione classe Mezzo

---
+++ EsameRecuperoDebito_FerrariGiacomo/EsameRecuperoDebito_FerrariGiacomo/Mezzo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsameRecuperoDebito_FerrariGiacomo
+{
+    internal class Mezzo
+    {
+
+    }
+}
