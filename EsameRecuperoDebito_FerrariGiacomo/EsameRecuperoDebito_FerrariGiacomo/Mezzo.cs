894698b124a3425178284b720ce56e8ca991ce3e
Pascal Informatica <informatica.pascal@comandini.local>
2025-09-02 15:30:57 +0200

    Classe Mezzo 1.0

--- EsameRecuperoDebito_FerrariGiacomo/EsameRecuperoDebito_FerrariGiacomo/Mezzo.cs
+++ EsameRecuperoDebito_FerrariGiacomo/EsameRecuperoDebito_FerrariGiacomo/Mezzo.cs
@@ -8,6 +8,24 @@ namespace EsameRecuperoDebito_FerrariGiacomo
 {
     internal class Mezzo
     {
+        string targa;
+        double prezzo;
 
+        class Macchina
+        {
+            int numeroPosti;
+            // Relativi metodi classe Macchina
+        }
+        class Moto
+        {
+            int cilindrata; // in cc (cm³)
+            // Relativi metodi classe Moto
+        }
+        class Camion
+        {
+            int carico; // in kg
+            // Relativi metodi classe Camion
+        }
+        // Relativi metodi classe Mezzo
     }
 }
